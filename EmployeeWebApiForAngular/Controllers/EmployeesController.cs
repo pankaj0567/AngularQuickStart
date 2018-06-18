c8df55c825ded5b7b797498ff6873f5089d23918
pankaj <AIOTechnicalPankaj@gmail.com>
2018-06-18 21:06:11 +0530

    Asp.Net Core Web Api
    Created asp.net core web api for angular  to consume it

---
+++ EmployeeWebApiForAngular/Controllers/EmployeesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeWebApiForAngular.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeWebApiForAngular.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Employees")]
+    public class EmployeesController : Controller
+    {
+        /// <summary>
+        /// This method is for getting all data of employee created by pankaj date : 18/06/2018
+        /// </summary>
+        /// <returns>return list of employee</returns>
+        [HttpGet]
+        public IEnumerable<Employees> GetEmployee()
+        {
+            using (EmployeeDbContextContext employeeDbContextContext = new EmployeeDbContextContext())
+            {
+                 return employeeDbContextContext.Employees.ToList();
+            }
+        }
+
+        /// <summary>
+        /// This method is for getting single data of employee created by pankaj date : 18/06/2018
+        /// </summary>
+        /// <param name="id">used to pass employee code</param>
+        /// <returns>return single employee data</returns>
+        [HttpGet]
+        [Route("{code}")]
+        public Employees GetEmployee(string code)
+        {
+            using (EmployeeDbContextContext employeeDbContextContext = new EmployeeDbContextContext())
+            {
+                return employeeDbContextContext.Employees.FirstOrDefault(emp => emp.Code == code);
+            }
+        }
+    }
+}
---
+++ EmployeeWebApiForAngular/Models/EmployeeDbContextContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EmployeeWebApiForAngular.Models
+{
+    public partial class EmployeeDbContextContext : DbContext
+    {
+        public EmployeeDbContextContext()
+        {
+        }
+
+        public EmployeeDbContextContext(DbContextOptions<EmployeeDbContextContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employees> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Server=LENOVOG50-80;Database=EmployeeDbContext;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employees>(entity =>
+            {
+                entity.HasKey(e => e.Code);
+
+                entity.Property(e => e.Code)
+                    .HasColumnName("code")
+                    .HasMaxLength(50)
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.AnnualSalary)
+                    .HasColumnName("annualSalary")
+                    .HasColumnType("decimal(18, 3)");
+
+                entity.Property(e => e.DateOfBirth)
+                    .HasColumnName("dateOfBirth")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Gender)
+                    .HasColumnName("gender")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(50);
+            });
+        }
+    }
+}
